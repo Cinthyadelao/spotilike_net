3d5b8a37f80f033155ca1d860606862cf09a9eb9
Diana <fdv.fedorova@gmail.com>
2024-09-11 22:23:13 +0200

    modif

---
+++ Spotilike/DTOs/AlbumDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace Spotilike.DTOs
+{
+    public class AlbumDTO
+    {
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        public string DateSortie { get; set; }
+        public int? ArtisteId { get; set; }
+        public List<MorceauDTO> Morceaux { get; set; } // Liste des morceaux associés
+    }
+
+    public class MorceauDTO
+    {
+        public string Titre { get; set; }
+        public string Duree { get; set; }
+        public int GenreId { get; set; }
+    }
+}
